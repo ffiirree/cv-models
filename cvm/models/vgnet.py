7a2599875bec7b2128d2975e63e76d7583960e86
ffiirree <ice_qi@163.com>
2021-11-30 09:51:31 +0800

    vgnet_g_1_0mp_se weights

--- cvm/models/vgnet.py
+++ cvm/models/vgnet.py
@@ -173,6 +173,7 @@ def vgnet_g_1_0mp(pretrained: bool = False, pth: str = None, progress: bool = Tr
 
 
 @export
+@config(url='https://github.com/ffiirree/cv-models/releases/download/v0.0.2-vgnets-weights/vgnet_g_1_0mp_se-1b12c66e.pth')
 def vgnet_g_1_0mp_se(pretrained: bool = False, pth: str = None, progress: bool = True, **kwargs: Any):
     kwargs['channels'] = [28, 56, 112, 224, 368]
     kwargs['downsamplings'] = ['blur', 'blur', 'blur', 'blur']
